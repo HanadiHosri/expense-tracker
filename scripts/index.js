70e154d97e3f1eb597c3935f130c40ae42bcc65c
unknown <hanadi.hosri@gmail.com>
2024-03-09 19:05:37 +0200

    adding addTransaction function

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 const selectCurrency = document.getElementById("currency");
+let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 
 fetch("https://ivory-ostrich-yoke.cyclic.app/students/available")
     .then(response => response.json())
@@ -11,3 +12,14 @@ fetch("https://ivory-ostrich-yoke.cyclic.app/students/available")
         });
     })
     .catch(error => console.error(error));
+
+function addTransaction() {
+    let amount = document.getElementById("amount").value;
+    let currency = document.getElementById("currency").value;
+    let type = document.getElementById("type").value;
+
+    let newTransaction = {amount, currency, type};
+    transactions.push(newTransaction);
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+    
+}
