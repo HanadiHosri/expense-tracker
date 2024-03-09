2e3964895e7dfb03eff2168153f9c39b45a92cf4
unknown <hanadi.hosri@gmail.com>
2024-03-09 20:18:55 +0200

    adding viewTransactions function

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 const selectCurrency = document.getElementById("currency");
+const tableBody = document.querySelector(".view-section table");
 let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 
 fetch("https://ivory-ostrich-yoke.cyclic.app/students/available")
@@ -22,4 +23,45 @@ function addTransaction() {
     transactions.push(newTransaction);
     localStorage.setItem("transactions", JSON.stringify(transactions));
     
-}
+    viewTransactions()
+};
+
+function viewTransactions() {
+    while (tableBody.rows.length > 1) {
+        tableBody.deleteRow(1);
+    };
+
+    transactions.forEach((transaction, index) => {
+        const row = tableBody.insertRow();
+        const amountCell = row.insertCell(0);
+        const currencyCell = row.insertCell(1);
+        const typeCell = row.insertCell(2);
+        const editRemoveCell = row.insertCell(3);
+
+        amountCell.textContent = transaction.amount;
+        currencyCell.textContent = transaction.currency;
+        typeCell.textContent = transaction.type;
+
+        const editButton = document.createElement("button");
+        const editIcon = document.createElement("i");
+        editIcon.className = "fas fa-edit";
+        editButton.appendChild(editIcon);
+
+        const removeButton = document.createElement("button");
+        const trashIcon = document.createElement("i");
+        trashIcon.className = "fas fa-trash-alt";
+        removeButton.appendChild(trashIcon);
+        removeButton.addEventListener("click", () => removeTransaction(index));
+
+        editRemoveCell.appendChild(editButton);
+        editRemoveCell.appendChild(removeButton);
+    });
+};
+
+viewTransactions();
+
+function removeTransaction(index) {
+    transactions.splice(index, 1);
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+    viewTransactions();
+};
